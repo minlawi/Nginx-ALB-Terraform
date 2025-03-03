a43835a66be184e76519261d0cab4935d0fa966b
minlawi <minlawi1992@gmail.com>
2025-03-03 23:52:53 +0800

    Modified security_group.tf

--- security_group.tf
+++ security_group.tf
@@ -1,15 +1,17 @@
-# resource "aws_security_group" "web_sg" {
-#   vpc_id = aws_vpc.nginx_vpc.id
-#   name   = "web_sg"
-# }
+resource "aws_security_group" "web_sg" {
+  count  = var.create_vpc ? 1 : 0
+  vpc_id = aws_vpc.nginx_vpc[0].id
+  name   = "web_sg"
+}
 
-# resource "aws_vpc_security_group_ingress_rule" "allow_http" {
-#   security_group_id = aws_security_group.web_sg.id
-#   cidr_ipv4         = local.anywhere
-#   from_port         = local.http
-#   to_port           = local.http
-#   ip_protocol       = local.tcp_protocol
-# }
+resource "aws_vpc_security_group_ingress_rule" "allow_http" {
+  count             = var.create_vpc ? 1 : 0
+  security_group_id = aws_security_group.web_sg[0].id
+  cidr_ipv4         = local.anywhere
+  from_port         = local.http
+  to_port           = local.http
+  ip_protocol       = local.tcp_protocol
+}
 
 # # resource "aws_vpc_security_group_ingress_rule" "allow_ssh" {
 # #   security_group_id = aws_security_group.web_sg.id
@@ -19,8 +21,9 @@
 # #   ip_protocol       = local.tcp_protocol
 # # }
 
-# resource "aws_vpc_security_group_egress_rule" "allow_all_traffic" {
-#   security_group_id = aws_security_group.web_sg.id
-#   cidr_ipv4         = local.anywhere
-#   ip_protocol       = local.all_ports
-# }
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic" {
+  count             = var.create_vpc ? 1 : 0
+  security_group_id = aws_security_group.web_sg[0].id
+  cidr_ipv4         = local.anywhere
+  ip_protocol       = local.all_ports
+}
