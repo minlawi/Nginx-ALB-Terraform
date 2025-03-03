857ec0daf6a5a6d7632224bb9d951e5e1ec58a14
minlawi <minlawi1992@gmail.com>
2025-03-03 20:59:24 +0800

    Added security_group.tf

---
+++ security_group.tf
@@ -0,0 +1,25 @@
+locals {
+  anywhere     = "0.0.0.0/0"
+  http         = 80
+  tcp_protocol = "tcp"
+  all_ports    = -1
+}
+
+resource "aws_security_group" "web_sg" {
+  vpc_id = aws_vpc.nginx_vpc.id
+  name   = "web_sg"
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_http" {
+  security_group_id = aws_security_group.web_sg.id
+  cidr_ipv4         = local.anywhere
+  from_port         = local.http
+  to_port           = local.http
+  ip_protocol       = local.tcp_protocol
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic" {
+  security_group_id = aws_security_group.web_sg.id
+  cidr_ipv4         = local.anywhere
+  ip_protocol       = local.all_ports
+}
