cd4660232934202d6abd83819444578107409e04
minlawi <minlawi1992@gmail.com>
2025-03-03 23:13:41 +0800

    Updated subnet_cidr_block

--- networking.tf
+++ networking.tf
@@ -10,7 +10,7 @@ resource "aws_vpc" "nginx_vpc" {
 # Subnets (Public)
 resource "aws_subnet" "public_subnet_1" {
   vpc_id                  = aws_vpc.nginx_vpc.id
-  cidr_block              = "10.0.1.0/24"
+  cidr_block              = "10.0.0.0/20"
   map_public_ip_on_launch = true
   availability_zone       = "ap-southeast-1a"
 
@@ -21,7 +21,7 @@ resource "aws_subnet" "public_subnet_1" {
 
 resource "aws_subnet" "public_subnet_2" {
   vpc_id                  = aws_vpc.nginx_vpc.id
-  cidr_block              = "10.0.2.0/24"
+  cidr_block              = "10.0.16.0/20"
   map_public_ip_on_launch = true
   availability_zone       = "ap-southeast-1b"
 
