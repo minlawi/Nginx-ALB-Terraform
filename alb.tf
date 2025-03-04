8032969b5693c3ad24d3baa1603f8c6adbee6ea2
minlawi <minlawi1992@gmail.com>
2025-03-04 19:48:14 +0800

    Modified the alb.tf

--- alb.tf
+++ alb.tf
@@ -1,15 +1,4 @@
-resource "aws_alb" "app_lb" {
-  count              = var.create_vpc ? 1 : 0
-  load_balancer_type = "application"
-  name               = "nginx-alb"
-  internal           = false
-  security_groups    = [aws_security_group.web_sg[0].id]
-  subnets            = aws_subnet.public_subnet[*].id
-  tags = {
-    name = "nginx-alb"
-  }
-}
-
+# 1. Create Target Group
 resource "aws_lb_target_group" "app_tg" {
   count       = var.create_vpc ? 1 : 0
   name        = "nginx-tg"
@@ -22,6 +11,7 @@ resource "aws_lb_target_group" "app_tg" {
   }
 }
 
+# 2. Register Targets
 resource "aws_lb_target_group_attachment" "tg_attachment" {
   count            = var.create_vpc ? length(aws_instance.nginx_instances) : 0
   target_group_arn = aws_lb_target_group.app_tg[0].arn
@@ -29,6 +19,20 @@ resource "aws_lb_target_group_attachment" "tg_attachment" {
   port             = local.http_port
 }
 
+# 3. Create Application Load Balancer
+resource "aws_alb" "app_lb" {
+  count              = var.create_vpc ? 1 : 0
+  load_balancer_type = "application"
+  name               = "nginx-alb"
+  internal           = false
+  security_groups    = [aws_security_group.web_sg[0].id]
+  subnets            = aws_subnet.public_subnet[*].id
+  tags = {
+    name = "nginx-alb"
+  }
+}
+
+# 4. Create Listener
 resource "aws_lb_listener" "listener" {
   count             = var.create_vpc ? 1 : 0
   load_balancer_arn = aws_alb.app_lb[0].arn
